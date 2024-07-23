1e788bdb57437e6b9eb2755561b7a95eff32bf0a
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-07-23 00:55:51 -0700

    Create model_aggregation.jl

---
+++ model_training/model_aggregation.jl
@@ -0,0 +1,14 @@
+using Distributed
+
+function model_aggregation(data::Vector{Vector{Float64}})
+    aggregated_model = mean(data, dims=1)
+    return aggregated_model
+end
+
+function main()
+    models = [rand(10) for _ in 1:4]
+    aggregated_model = model_aggregation(models)
+    println("Aggregated model: $aggregated_model")
+end
+
+main()
